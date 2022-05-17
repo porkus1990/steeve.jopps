55a886ed2e44bd23341e9c969febbcd0f30cf9a9
Martin B <porkus1990@googlemail.com>
2022-05-17 16:48:39 +0000

    chore: add logout button for testing

--- dm-jobs/web/src/Routes.tsx
+++ dm-jobs/web/src/Routes.tsx
@@ -1,4 +1,4 @@
-import { Set, Router, Route } from '@redwoodjs/router'
+import { Private, Set, Router, Route } from '@redwoodjs/router'
 import JobsLayout from 'src/layouts/JobsLayout'
 
 const Routes = () => {
@@ -8,12 +8,14 @@ const Routes = () => {
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      <Set wrap={JobsLayout}>
-        <Route path="/jobs/new" page={JobNewJobPage} name="newJob" />
-        <Route path="/jobs/{id:Int}/edit" page={JobEditJobPage} name="editJob" />
-        <Route path="/jobs/{id:Int}" page={JobJobPage} name="job" />
-        <Route path="/jobs" page={JobJobsPage} name="jobs" />
-      </Set>
+      <Private unauthenticated="home">
+        <Set wrap={JobsLayout}>
+          <Route path="/jobs/new" page={JobNewJobPage} name="newJob" />
+          <Route path="/jobs/{id:Int}/edit" page={JobEditJobPage} name="editJob" />
+          <Route path="/jobs/{id:Int}" page={JobJobPage} name="job" />
+          <Route path="/jobs" page={JobJobsPage} name="jobs" />
+        </Set>
+      </Private>
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
