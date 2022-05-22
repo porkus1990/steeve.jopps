342804018a4cd2d9e97a0d04eaf121e33a4b0885
Martin B <porkus1990@googlemail.com>
2022-05-22 14:50:07 +0000

    feat: add login - logout

--- dm-jobs/web/src/pages/HomePage/HomePage.tsx
+++ dm-jobs/web/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,14 @@
 import { Link, routes } from '@redwoodjs/router';
 import { MetaTags } from '@redwoodjs/web';
 import { useAuth } from '@redwoodjs/auth';
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Map } from '../../components/Map';
 import './homepage.scss';
@@ -27,11 +34,24 @@ const HomePage = () => {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Steeve.jopps
             </Typography>
-            <Link to={routes.login()} className="homepage__login">
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Login
-              </Typography>
-            </Link>
+            {isAuthenticated ? (
+              <Button onClick={logOut}>
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ flexGrow: 1 }}
+                  className="homepage homepage__logout"
+                >
+                  Logout
+                </Typography>
+              </Button>
+            ) : (
+              <Link to={routes.login()} className="homepage homepage__login">
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  Login
+                </Typography>
+              </Link>
+            )}
           </Toolbar>
         </AppBar>
       </Box>
